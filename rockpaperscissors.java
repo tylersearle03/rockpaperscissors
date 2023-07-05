9f6e2d5c1ae016819e3602b32d24176d59a86401
Tyler <tylersearle003@gmail.com>
2023-07-05 01:48:24 -0500

    created a system that decides who wins once the player has chosen their move

--- rockpaperscissors.java
+++ rockpaperscissors.java
@@ -10,17 +10,48 @@ public class rockpaperscissors {
         String playerMove;
 
         while(true) {
-        System.out.println("Please enter yout move (r, p, or s)");
+        System.out.println("Please enter your move (r, p, or s)");
         playerMove = scanner.nextLine(); 
         if (playerMove.equals("r") || playerMove.equals("p") || playerMove.equals("s")) {
             break;
         }
+        System.out.println(playerMove + " is not a valid move.");
 
      }
 
+     System.out.println("Computer played: " + computerMove);
+    
+     if (playerMove.equals(computerMove)) {
+       System.out.println("The game was a tie!");
+     }
+     else if (playerMove.equals("r")) {
+       if (computerMove.equals("p")) {
+          System.out.println("You Lose!");
+
+        } else if (computerMove.equals("s")){
+            System.out.println("You Win!");
+        }
+     }
+
+     else if (playerMove.equals("p")) {
+        if (computerMove.equals("r")) {
+           System.out.println("You win!");
+ 
+         } else if (computerMove.equals("s")){
+             System.out.println("You Lose!");
+         }
+      }
+
+      else if (playerMove.equals("s")) {
+        if (computerMove.equals("p")) {
+           System.out.println("You win!");
+ 
+         } else if (computerMove.equals("r")){
+             System.out.println("You Lose!");
+         }
+      }
 
         
 
   }
 }
-;
